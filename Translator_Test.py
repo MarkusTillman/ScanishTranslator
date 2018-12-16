199996feaf5bdce967512c3d4d6445420a367d47
Markus Tillman <markustillman91@gmail.com>
2018-12-16 11:45:09 +0100

    Make translation for swedish "och" unambigious
    "Och" in swedish was translated to "å" in scanish which can also mean
    "river". Changed the translation to "o" and move it to twoWay.dict.

--- Translator_Test.py
+++ Translator_Test.py
@@ -25,8 +25,8 @@ class TestTranslateScanishToSwedish:
         assert Translator.toSwedish("Jävla ålahue jau ei så ked på daj") == "Jävla dumhuvud jag är så trött på dig"
 
     def testThatSeveralWordsDividedBySpecialCharactersCanBeTranslated(self):
-        scanish = "Jävla ålahue; jau. ei. så, ked/putt på daj, å dina päron!"
-        assert Translator.toSwedish(scanish) == "Jävla dumhuvud; jag. är. så, trött/irriterad på dig, å dina föräldrar!"
+        scanish = "Jävla ålahue; jau. ei. så, ked/putt på daj, o dina päron!"
+        assert Translator.toSwedish(scanish) == "Jävla dumhuvud; jag. är. så, trött/irriterad på dig, och dina föräldrar!"
 
 class TestTranslateSwedishToScanish:
     
@@ -50,11 +50,11 @@ class TestTranslateSwedishToScanish:
         assert Translator.toScanish("WordWithoutTranslation") == "WordWithoutTranslation"
 
     def testThatWordWithOnlyOneWayTranslationIsTranslated(self):
-        assert Translator.toScanish("och") == "å"
+        assert Translator.toScanish("hej") == "haj"
 
     def testThatSeveralWordsCanBeTranslatedAtOnce(self):
         assert Translator.toScanish("Jävla dumhuvud jag är så trött på dig") == "Jävla ålahue jau ei så ked på daj"
 
     def testThatSeveralWordsDividedBySpecialCharactersCanBeTranslated(self):
         swedish = "Jävla dumhuvud; jag. är. så, trött/irriterad på dig, och dina föräldrar!"
-        assert Translator.toScanish(swedish) == "Jävla ålahue; jau. ei. så, ked/putt på daj, å dina päron!"
+        assert Translator.toScanish(swedish) == "Jävla ålahue; jau. ei. så, ked/putt på daj, o dina päron!"
