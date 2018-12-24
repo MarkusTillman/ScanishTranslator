14f510d63370388208bdf82f5e40e2d2f61b91e1
Markus Tillman <markustillman91@gmail.com>
2018-12-24 18:47:48 +0100

    Integrate with slack message events
    Slack events are now picked up by the handleEvents()-method, expecting
    HTTP post requests with JSON payload.
    
    200 (OK) response shall always be returned, just to let Slack know the
    server recieved the request. The only exception to this is when
    validation the URL to the server through Slack, which sends a
    "challenge"-value, expecting it in the response.
    
    All user's texts (in the scope; channels as configured in the "My apps"
    page at Slack) are now translated to Swedish by default by attaching it
    to the original message. This is not done for the invocation of commands
    (where the test is prefixed with '/'). Messages with a subtype (as
    message_changed, for instance) is not modified as message events are
    triggered when the server posts a change.

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import argparse
 import Translator
 import requests
+from ChatData import ChatData
+import sys
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 argumentParser = argparse.ArgumentParser()
 argumentParser.add_argument("--scanish", help="Scanish text to be translated to Swedish")
 argumentParser.add_argument("--swedish", help="Swedish text to be translated to Scanish")
+accessToken = open("access.token", "r").read()
 
 def parseText(text):
     command = text.split(" ")[0]
@@ -45,3 +48,68 @@ def handleCommand():
         return response(translatedText)
     except:
         return errorResponse(argumentParser.format_usage())
+
+
+@app.route("/", methods=["POST"])
+def handleEvents():
+    try:
+        jsonData = request.get_json()
+        print("Received: \n" + str(jsonData))
+        if not jsonData:
+            print("Expected JSON")
+            return okResponse()
+
+        if "challenge" in jsonData:
+            return jsonify({ "challenge" : jsonData["challenge"] })
+        else:
+            translateAndSend(jsonData) # todo: make asynchronously
+        return okResponse()
+    except:
+        print("Unexpected error:", sys.exc_info())
+        return okResponse()
+
+def okResponse():
+    print("Returning ok response")
+    return ""
+
+def isCommand(text):
+    return text[0] == '/'
+
+
+def translateAndSend(jsonData):
+    if "event" not in jsonData:
+        return
+    event = jsonData["event"]
+    if "type" not in event: 
+        return
+    if event["type"] != "message":
+        return 
+    if "subtype" in event:
+        return
+    text = event["text"]
+    if (isCommand(text)):
+        return
+
+    arguments = parseText("--scanish " + text) # todo: make configuration using new command.
+    translatedText = translateText(arguments)
+    dataToUpdateChatWith = ChatData(
+        token = jsonData["token"], 
+        channel = event["channel"], 
+        originalText = text, 
+        timestamp = event["ts"], 
+        translatedText = translatedText)
+    json = {
+        "token": dataToUpdateChatWith.token,
+        "channel": dataToUpdateChatWith.channel,
+        "text": dataToUpdateChatWith.originalText,
+        "ts": dataToUpdateChatWith.timestamp,
+        "attachments": [
+            {
+                "text": dataToUpdateChatWith.translatedText
+            }
+        ]
+    }
+    headers = {"Authorization": "Bearer " + accessToken}
+    print("Posting: \n" + str(headers) + "\n" + str(json))
+    response = requests.post("https://slack.com/api/chat.update", headers = headers, json = json)
+    print("Post response: \n" + str(response.content))
