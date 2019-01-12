eec1cca4b4bfc8b0cf70603b1c346d823df13082
Markus Tillman <markustillman91@gmail.com>
2019-01-12 16:11:27 +0100

    Extract handling of subscribed slack events to class

---
+++ EventOperation.py
@@ -0,0 +1,91 @@
+import logging
+import sys
+import UserStorage
+import argparse
+import Translator
+import _thread
+from ChatData import ChatData
+import RequestHandler
+import RequestSender
+
+eventArgumentParser = argparse.ArgumentParser()
+eventArgumentParser.add_argument("--scanish", help="Scanish text to be translated to Swedish")
+eventArgumentParser.add_argument("--swedish", help="Swedish text to be translated to Scanish")
+
+def handle(request):
+    try:
+        RequestHandler.verifyRequest(request)
+        jsonData = request.get_json()
+        if not jsonData:
+            logging.warning("Expected JSON data")
+            return ""
+
+        if "challenge" in jsonData:
+            return jsonify({ "challenge" : jsonData["challenge"] })
+        elif shallTranslate(jsonData):
+            letNewThreadHandleTheTranslation(jsonData)
+        return ""
+    except:
+        logging.error("Unexpected error: " + str(sys.exc_info()))
+        return ""
+
+def parseArguments(text, argumentParser):
+    argumentName = text.split(" ")[0]
+    argumentValue = text[len(argumentName) + 1:]
+    return argumentParser.parse_args([argumentName, argumentValue])
+
+def translateText(arguments):
+    if arguments.scanish:
+        return Translator.toSwedish(arguments.scanish)
+    elif arguments.swedish:
+        return Translator.toScanish(arguments.swedish)
+    else:
+        return "No text to translate"
+
+def isCommand(text):
+    return text[0] == '/'
+
+def shallTranslate(jsonData):
+    if "event" not in jsonData:
+        return False
+    event = jsonData["event"]
+    if "user" not in event:
+        return False
+    if "type" not in event: 
+        return False
+    if event["type"] != "message":
+        return False
+    if "subtype" in event:
+        return False
+    if isCommand(event["text"]):
+        return False
+    if not UserStorage.isRegisteredUser(event["user"]):
+        return False
+    return True
+
+def letNewThreadHandleTheTranslation(jsonData):
+    _thread.start_new_thread(doTheTranslation, (jsonData, ))
+
+def doTheTranslation(jsonData):
+    try:
+        translatedText = translate(jsonData)
+        chatData = createChatData(jsonData, translatedText)
+        RequestSender.send(chatData)
+    except:
+        logging.error("Unexpected error: " + str(sys.exc_info()))
+
+def translate(jsonData):
+    text = jsonData["event"]["text"]
+    userId = jsonData["event"]["user"]
+    arguments = parseArguments("--" + UserStorage.getTranslationModeFor(userId) + " " + text, eventArgumentParser)
+    return translateText(arguments)
+
+def createChatData(jsonData, translatedText):
+    event = jsonData["event"]
+    return ChatData(
+        token = jsonData["token"], 
+        channel = event["channel"], 
+        originalText = event["text"], 
+        timestamp = event["ts"], 
+        userId = event["user"],
+        translatedText = translatedText)
--- app.py
+++ app.py
@@ -1,35 +1,14 @@
-import argparse
-import Translator
-import RequestHandler
-import RequestSender
-import UserStorage
+
+
+import logging
+from flask import Flask, request
+
 import AuthorizationOperation
 import CommandOperation
-import logging
-from ChatData import ChatData
-import sys
-from flask import Flask, request
-import _thread
+import EventOperation
 
 app = Flask(__name__)
 logging.basicConfig(filename="log.log", level=logging.DEBUG, filemode="w")
-
-eventArgumentParser = argparse.ArgumentParser()
-eventArgumentParser.add_argument("--scanish", help="Scanish text to be translated to Swedish")
-eventArgumentParser.add_argument("--swedish", help="Swedish text to be translated to Scanish")
-
-def parseArguments(text, argumentParser):
-    argumentName = text.split(" ")[0]
-    argumentValue = text[len(argumentName) + 1:]
-    return argumentParser.parse_args([argumentName, argumentValue])
-
-def translateText(arguments):
-    if arguments.scanish:
-        return Translator.toSwedish(arguments.scanish)
-    elif arguments.swedish:
-        return Translator.toScanish(arguments.swedish)
-    else:
-        return "No text to translate"
 
 def logReceivedRequest(request):
     logging.info("Received: " + str(request.headers) + str(request.get_data()))
@@ -50,68 +29,6 @@ def handleSlackCommand():
     return CommandOperation.handle(request)
 
 @app.route("/", methods=["POST"])
-def handleJsonEvents():
+def handleSubscribedSlackEvents():
     logReceivedRequest(request)
-    try:
-        RequestHandler.verifyRequest(request)
-        jsonData = request.get_json()
-        if not jsonData:
-            logging.warning("Expected JSON data")
-            return ""
-
-        if "challenge" in jsonData:
-            return jsonify({ "challenge" : jsonData["challenge"] })
-        elif shallTranslate(jsonData):
-            letNewThreadHandleTheTranslation(jsonData)
-        return ""
-    except:
-        logging.error("Unexpected error: " + str(sys.exc_info()))
-        return ""
-
-def isCommand(text):
-    return text[0] == '/'
-
-def shallTranslate(jsonData):
-    if "event" not in jsonData:
-        return False
-    event = jsonData["event"]
-    if "user" not in event:
-        return False
-    if "type" not in event: 
-        return False
-    if event["type"] != "message":
-        return False
-    if "subtype" in event:
-        return False
-    if isCommand(event["text"]):
-        return False
-    if not UserStorage.isRegisteredUser(event["user"]):
-        return False
-    return True
-
-def letNewThreadHandleTheTranslation(jsonData):
-    _thread.start_new_thread(doTheTranslation, (jsonData, ))
-
-def doTheTranslation(jsonData):
-    try:
-        translatedText = translate(jsonData)
-        chatData = createChatData(jsonData, translatedText)
-        RequestSender.send(chatData)
-    except:
-        logging.error("Unexpected error: " + str(sys.exc_info()))
-
-def translate(jsonData):
-    text = jsonData["event"]["text"]
-    userId = jsonData["event"]["user"]
-    arguments = parseArguments("--" + UserStorage.getTranslationModeFor(userId) + " " + text, eventArgumentParser)
-    return translateText(arguments)
-
-def createChatData(jsonData, translatedText):
-    event = jsonData["event"]
-    return ChatData(
-        token = jsonData["token"], 
-        channel = event["channel"], 
-        originalText = event["text"], 
-        timestamp = event["ts"], 
-        userId = event["user"],
-        translatedText = translatedText)
+    return EventOperation.handle(request)
