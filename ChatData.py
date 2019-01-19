8a775e56050bc944f32b8848b4fa04efc5a872d5
Markus Tillman <markustillman91@gmail.com>
2019-01-19 13:16:09 +0100

    Add comparison and string representation methods to ChatData
    Data strukturs (like ChatData) shall be comparable and printable.

--- ChatData.py
+++ ChatData.py
@@ -6,3 +6,9 @@ class ChatData:
         self.timestamp = timestamp
         self.userId = userId
         self.translatedText = translatedText
+
+    def __eq__(self, other): 
+        return self.token == other.token and self.channel == other.channel and self.originalText == other.originalText and self.timestamp == other.timestamp and self.userId == other.userId and self.translatedText == other.translatedText
+
+    def __str__(self):
+        return F"{self.token} {self.channel} {self.originalText} {self.timestamp} {self.userId} {self.translatedText}"
