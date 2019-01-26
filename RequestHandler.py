b14af6e0f4d2f39de5a4566f99fab8b49d1f7a22
Markus Tillman <markustillman91@gmail.com>
2019-01-26 11:54:15 +0100

    Fix "challenge"-request handling
    To verify an URL, slack sends a request with a "challenge"-token,
    expecting it back in a response. This does not work because of request
    verification which assumes a given format.
    
    Verification is no longer done on challenge-requests.

--- RequestHandler.py
+++ RequestHandler.py
@@ -5,10 +5,15 @@ import SignatureComputer
 binarySigningSecret = open("signing.secret", "rb").read()
 
 def verifySlackRequest(request):
-    receivedSignature = request.headers["X-Slack-Signature"]
-    timestamp = request.headers["X-Slack-Request-Timestamp"] 
-    rawBody = request.get_data()
-    computedSignature = SignatureComputer.createSlackSignature(timestamp, rawBody.decode("utf-8"), binarySigningSecret)
-    if not SignatureComputer.verifySignaturesAreSame(computedSignature, receivedSignature):
-        logging.warning("Verification failed. Computed signature: " + computedSignature)
-        abort(400)
+    if not isUrlVerification(request):
+        receivedSignature = request.headers["X-Slack-Signature"]
+        timestamp = request.headers["X-Slack-Request-Timestamp"] 
+        rawBody = request.get_data()
+        computedSignature = SignatureComputer.createSlackSignature(timestamp, rawBody.decode("utf-8"), binarySigningSecret)
+        if not SignatureComputer.verifySignaturesAreSame(computedSignature, receivedSignature):
+                logging.warning("Verification failed. Computed signature: " + computedSignature)
+                abort(400)
+
+def isUrlVerification(request):
+    jsonData = request.get_json()
+    return jsonData is not None and "type" in jsonData and jsonData["type"] == "url_verification" and "challenge" in jsonData
--- RequestHandler_Test.py
+++ RequestHandler_Test.py
@@ -8,25 +8,30 @@ class TestRequestHandler(unittest.TestCase):
     def testThatSignatureHeaderIsRequired(self):
         request = Mock()
         request.headers = {}
+        request.get_json.return_value = {}
         self.assertRaisesRegex(KeyError, "X-Slack-Signature", RequestHandler.verifySlackRequest, request)
     
     def testThatTimestampHeaderIsRequired(self):
-        request = Mock()
-        request.headers = {
+        request = mockRequest(data = b"body", headers = {
             "X-Slack-Signature": "abc"
-        }
+        })
         self.assertRaisesRegex(KeyError, "X-Slack-Request-Timestamp", RequestHandler.verifySlackRequest, request)
 
     @patch('logging.warning')
     @patch('SignatureComputer.createSlackSignature')
     def testThatWarningIsLoggedAndBadRequestIsThrownWhenSignatureDoesNotMatch(self, createSlackSignatureMock, warningMock):
-        request = Mock()
-        request.headers = {
+        request = mockRequest(data = b"body", headers = {
             "X-Slack-Signature": "abc",
             "X-Slack-Request-Timestamp": "123"
-        }
-        request.get_data = lambda: b"body"
+        })
         createSlackSignatureMock.return_value = "cba"
         
         self.assertRaisesRegex(Exception, "400 Bad Request.*", RequestHandler.verifySlackRequest, request)
         warningMock.assert_called()
+
+def mockRequest(headers=None, json={}, data=None):
+    request = Mock()
+    request.get_json.return_value = json
+    request.headers = headers
+    request.get_data = lambda: data
+    return request
